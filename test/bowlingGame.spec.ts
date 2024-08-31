c094fb47fc5313a06030dfc0eef0cd14e0b078d1
takumab <takumab@gmail.com>
2024-08-31 17:14:11 +0100

    T, should knock down 1 pin on the first roll. (Nil -> constant)

--- test/bowlingGame.spec.ts
+++ test/bowlingGame.spec.ts
@@ -1,5 +1,26 @@
-describe('Sample Test', () => {
-    test("should do something", () => {
-        expect(1+1).toBe(2);
-    })
+// If, after both balls, there is still at least one of the ten
+// pins standing the score for that frame is simply the total
+class BowlingGame {
+  score(_game: string) {
+    return 1;
+  }
+}
+
+// number of pins knocked down in those two balls.
+describe("Bowling Game", () => {
+  describe("Acceptance", () => {
+    test("should bowl a game score of 90", () => {
+      let bowlingGame = new BowlingGame();
+      let result = bowlingGame.score("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||");
+      expect(result).toBe(90);
+    });
+  });
+
+  describe("Unit", () => {
+    test("should knocked down 1 pin on the first roll", () => {
+      let bowlingGame = new BowlingGame();
+      let result = bowlingGame.score("1");
+      expect(result).toBe(1);
+    });
+  });
 });
