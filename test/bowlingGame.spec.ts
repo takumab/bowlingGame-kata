eb64dbd087c2b0e7cdfb867f31d1e07e7d598383
takumab <takumab@gmail.com>
2024-08-31 21:38:49 +0100

    R, (Constant -> constant+)

--- test/bowlingGame.spec.ts
+++ test/bowlingGame.spec.ts
@@ -1,7 +1,7 @@
 class BowlingGame {
   score(game: string) {
     const calculateRolls = () => {
-      if (game === "1-") return 1;
+      if (game === "1-") return 1 + 0;
       if (game === "2-") return 2;
       if (game === "3-") return 3;
       let totalRolls = 0;
