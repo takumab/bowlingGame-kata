ebfa7c7aeea58892aa07eead021e6dd61b7869e5
takumab <takumab@gmail.com>
2024-08-31 17:49:38 +0100

    R, set up bowling game instance for each test/example

--- test/bowlingGame.spec.ts
+++ test/bowlingGame.spec.ts
@@ -25,32 +25,32 @@ describe("Bowling Game", () => {
   });
 
   describe("Unit", () => {
+    let bowlingGame: BowlingGame;
+    beforeEach(() => {
+      bowlingGame = new BowlingGame();
+    });
+
     test("should knock down 1 pin on the first roll", () => {
-      let bowlingGame = new BowlingGame();
       let result = bowlingGame.score("1");
       expect(result).toBe(1);
     });
 
     test("should knock down 2 pins on the first roll", () => {
-      let bowlingGame = new BowlingGame();
       let result = bowlingGame.score("2");
       expect(result).toBe(2);
     });
 
     test("should knock down 3 pins on the first roll", () => {
-      let bowlingGame = new BowlingGame();
       let result = bowlingGame.score("3");
       expect(result).toBe(3);
     });
 
     test("should knock down a total of 4 pins", () => {
-      let bowlingGame = new BowlingGame();
       let result = bowlingGame.score("13");
       expect(result).toBe(4);
     });
 
     test("should knock down a total of 5 pins", () => {
-      let bowlingGame = new BowlingGame();
       let result = bowlingGame.score("14");
       expect(result).toBe(5);
     });
