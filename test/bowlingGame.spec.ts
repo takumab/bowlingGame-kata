761233dd532edf0031914b1f81c1ed2992d8bdf2
takumab <takumab@gmail.com>
2024-09-04 00:15:44 +0100

    T, should know down a total of 18 pins

--- test/bowlingGame.spec.ts
+++ test/bowlingGame.spec.ts
@@ -2,6 +2,7 @@ class BowlingGame {
   score(game: string) {
     return this.calculateRollsFor(game);
   }
+
   private calculateRollsFor = (game: string) => {
     const rolls = game.split("|").join("");
     const GUTTER_BALL = "-";
@@ -20,10 +21,19 @@ class BowlingGame {
 
 describe("Bowling Game", () => {
   describe("Acceptance", () => {
+    let bowlingGame: BowlingGame;
+    beforeEach(() => {
+      bowlingGame = new BowlingGame();
+    });
+
     test("should bowl a game score of 90", () => {
-      let bowlingGame = new BowlingGame();
       let result = bowlingGame.score("9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||");
       expect(result).toBe(90);
+    });
+
+    test("should bowl a game score of 150", () => {
+      let result = bowlingGame.score("5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5");
+      expect(result).toBe(150);
     });
   });
 
@@ -34,21 +44,22 @@ describe("Bowling Game", () => {
     });
 
     test.each`
-      pins     | expectedResult
-      ${"1"}   | ${1}
-      ${"2"}   | ${2}
-      ${"3"}   | ${3}
-      ${"13"}  | ${4}
-      ${"14"}  | ${5}
-      ${"1-"}  | ${1}
-      ${"2-"}  | ${2}
-      ${"3-"}  | ${3}
-      ${"-1"}  | ${1}
-      ${"-2"}  | ${2}
-      ${"-3"}  | ${3}
-      ${"--"}  | ${0}
-      ${"12|"} | ${3}
-      ${"34|"} | ${7}
+      pins       | expectedResult
+      ${"1"}     | ${1}
+      ${"2"}     | ${2}
+      ${"3"}     | ${3}
+      ${"13"}    | ${4}
+      ${"14"}    | ${5}
+      ${"1-"}    | ${1}
+      ${"2-"}    | ${2}
+      ${"3-"}    | ${3}
+      ${"-1"}    | ${1}
+      ${"-2"}    | ${2}
+      ${"-3"}    | ${3}
+      ${"--"}    | ${0}
+      ${"12|"}   | ${3}
+      ${"34|"}   | ${7}
+      ${"34|56"} | ${18}
     `(
       "should knock down $pins pin(s) for a total of $expectedResult",
       ({ pins, expectedResult }) => {
