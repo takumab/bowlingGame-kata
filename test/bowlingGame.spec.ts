65674dc951894bf11987830fa1b0ffd11611974a
takumab <takumab@gmail.com>
2024-09-04 11:32:47 +0100

    R

--- test/bowlingGame.spec.ts
+++ test/bowlingGame.spec.ts
@@ -4,8 +4,8 @@ class BowlingGame {
   }
 
   private calculateRollsFor = (game: string) => {
-    if (game.includes("8/|34")) return 10 + 3 + 3 + 4;
-    if (game.includes("5/|24")) return 10 + 2 + 2 + 4;
+    if (game.includes("8/|34")) return 10 + 3 + (3 + 4);
+    if (game.includes("5/|24")) return 10 + 2 + (2 + 4);
     const rolls = game.split("|").join("");
     const GUTTER_BALL = "-";
 
