4adbce14afecf13c1cfc1122c4f4ef785c4999fc
takumab <takumab@gmail.com>
2024-09-04 11:43:34 +0100

    R, (Constant -> scalar)

--- test/bowlingGame.spec.ts
+++ test/bowlingGame.spec.ts
@@ -6,7 +6,7 @@ class BowlingGame {
   private calculateRollsFor = (game: string) => {
     if (game.includes("8/|34")) return 10 + Number(game[3]) + (3 + 4);
     if (game.includes("5/|24")) return 10 + Number(game[3]) + (2 + 4);
-    if (game.includes("7/|44")) return 10 + 4 + (4 + 4);
+    if (game.includes("7/|44")) return 10 + Number(game[3]) + (4 + 4);
     const rolls = game.split("|").join("");
     const GUTTER_BALL = "-";
 
